1ea8115552f250826554a16f05e46ac85a071aa7
Andrey Koval <akoval29@gmail.com>
2024-11-06 21:53:44 +0200

    done

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("10f1c1c577a98a75")
+/******/ 		__webpack_require__.h = () => ("21ec1711e12060e2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/news/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default function NewsDetailPage({ params }) {
-  const newsID = params.id;
-  return (
-    <>
-      <h1>News Detail Page</h1>
-      <p>News ID: {newsID}</p>
-    </>
-  );
-}
